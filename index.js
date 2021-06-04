87c7977fc96aaf707e1f6f67e8e783e5731a8282
Roland Levy <contact@rolandlevy.co.uk>
2021-06-04 22:44:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const redux = require('redux');
+const { createStore } = redux; 
+
+const BUY_CAKE = 'BUY_CAKE';
+
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  }
+}
+
+const initialState = {
+  numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
